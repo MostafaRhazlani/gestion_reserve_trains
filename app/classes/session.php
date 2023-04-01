ddc94c569d6021c21f8ac0cd53d13de4cefc7f6d
Othmane_Rhazlani <oghazlani0@gmail.com>
2023-04-01 23:06:50 +0100

    creet index, autoload, database, classes,

---
+++ app/classes/session.php
@@ -0,0 +1,8 @@
+<?php
+
+  class session{
+    static public function set($type, $message) {
+      setcookie($type, $message, time() + 5, '/');
+    }
+  }
+?>
--- database/DB.php
+++ database/DB.php
@@ -3,7 +3,7 @@
     class DB {
         static public function connect() {
             $db = new PDO("mysql:host=localhost;dbname=reserve_train", "root","");
-            $db->exec("set name utf8");
+            $db->exec("set names utf8");
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
             return $db;
         }
