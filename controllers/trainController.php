8a3df8df48687de8eb0f90e8715a0cdc5b99326c
Othmane_Rhazlani <oghazlani0@gmail.com>
2023-04-14 21:07:19 +0100

    edit controllers models train

--- controllers/trainController.php
+++ controllers/trainController.php
@@ -1,57 +1,86 @@
 <?php
 
     class trainController {
-        public function getAll() {
-            $trains = Train::getAll();
-            return $trains;
+
+        static public function index() {
+          $trains = Train::getAll();
+          include('./views/page/train/index.php');
         }
 
-        public function getByIdTrain() {
-            if(isset($_POST['id_train'])){
-                $data = array(
-                  'id_train' => $_POST['id_train']
-                );
-                
-                $train = Train::getByIdTrain($data);
-                return $train;
+        public function create() {
+          $trains = trainController::store();        
+          include('./views/page/train/add.php');
+        }
+
+        public function store() {
+          if(isset($_POST['submit'])) {
+            $data = array(
+              'name_train' => strtoupper($_POST['name_train']),
+              'photo' => $_FILES['photo']['name'],
+              'capacity' => $_POST['capacity'],
+            );
+
+            $imageTmp = $_FILES['photo']['tmp_name'];
+            $uploadfile = $_SERVER['DOCUMENT_ROOT'] . "/mostafa/gestion_reserve_train/public/images/" . $data['photo'];
+              
+            move_uploaded_file($imageTmp, $uploadfile);
+
+            $result = Train::store($data);
+            if($result === 'ok') {
+              session::set('success', 'Employé Ajouté');
+              redirect::to(BASE_URL.'train');
+
+            } else {
+              echo $result;
             }
+          }
+        }
+
+        public function edit() {
+          if(isset($_POST['id_train'])){
+              $data = array(
+                'id_train' => $_POST['id_train']
+              );
+              
+              $trains = Train::getByIdTrain($data);
+              include('./views/page/train/edit.php');
+          }
         }
 
         public function update() {
 
-            if(isset($_POST['submit'])) {
-              $data = array(
-                'id_train' => $_POST['id_train'],
-                'name_train' => strtoupper($_POST['name_train']),
-                'photo' => $_FILES['photo']['name'],
-                'capacity' => $_POST['capacity'],
-              );
+          if(isset($_POST['submit'])) {
+            $data = array(
+              'id_train' => $_POST['id_train'],
+              'name_train' => strtoupper($_POST['name_train']),
+              'photo' => $_FILES['photo']['name'],
+              'capacity' => $_POST['capacity'],
+            );
 
-              $imageTmp = $_FILES['photo']['tmp_name'];
-              $uploadfile = $_SERVER['DOCUMENT_ROOT'] . "/mostafa/gestion_reserve_train/public/images/" . $data['photo'];
-              
-              move_uploaded_file($imageTmp, $uploadfile);
-              
-              
-              $result = Train::update($data);
-              if($result === 'ok') {
-                session::set('success', 'The flight has been updated successfully');
-                redirect::to('?page=train');
-              } else {
-                echo $result;
-              }
+            $imageTmp = $_FILES['photo']['tmp_name'];
+            $uploadfile = $_SERVER['DOCUMENT_ROOT'] . "/mostafa/gestion_reserve_train/public/images/" . $data['photo'];
+            
+            move_uploaded_file($imageTmp, $uploadfile);
+            
+            
+            $result = Train::update($data);
+            if($result === 'ok') {
+              session::set('success', 'The flight has been updated successfully');
+              redirect::to(BASE_URL.'train');
+            } else {
+              echo $result;
             }
+          }
         }
-
+        
         public function delete() {
           if(isset($_POST['id_train'])) {
-          $data['id_train'] = $_POST['id_train'];
-
+            $data['id_train'] = $_POST['id_train'];
             $result = Train::delete($data);
 
             if($result === 'ok') {
               session::set('success', 'The flight has been deleted successfully');
-              redirect::to('?page=train');
+              redirect::to(BASE_URL.'train');
             } else {
               return $result;
             }
