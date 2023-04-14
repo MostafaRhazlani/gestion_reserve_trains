32fb6c324404c02044ab97a1e669f2e681da9f8d
Othmane_Rhazlani <oghazlani0@gmail.com>
2023-04-14 21:06:35 +0100

    add photos and edit in file js

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,39 @@
 // Get a reference to the select element
-// const departure_s = document.getElementById('departure_s');
-// const arrival_s = document.getElementById('arrival_s');
+const departure_s = document.getElementById('departure_s');
+const arrival_s = document.getElementById('arrival_s');
 
-// // Fetch data from API and fill select options
-// fetch('https://countriesnow.space/api/v0.1/countries/cities', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     country: 'morocco'
-//   })
-// })
-// .then(response => response.json())
-// .then(data => {
-//     fill_select(data);
-// })
-// .catch(error => {
-//   console.error('Error:', error);
-// });
+// Fetch data from API and fill select options
+fetch('https://countriesnow.space/api/v0.1/countries/cities', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    country: 'morocco'
+  })
+})
+.then(response => response.json())
+.then(data => {
+    fill_select(data);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
 
-// function fill_select(data) {
-//   data.data.forEach(city => {
-//     const option_departure_s = document.createElement('option');
-//     const option_arrival_s = document.createElement('option');
+function fill_select(data) {
+  data.data.forEach(city => {
+    const option_departure_s = document.createElement('option');
+    const option_arrival_s = document.createElement('option');
 
-//     option_departure_s.value = city;
-//     option_departure_s.textContent = city;
-//     departure_s.appendChild(option_departure_s);
+    option_departure_s.value = city;
+    option_departure_s.textContent = city;
+    departure_s.appendChild(option_departure_s);
 
-//     option_arrival_s.value = city;
-//     option_arrival_s.textContent = city;
-//     arrival_s.appendChild(option_arrival_s);
-//   });
-// }
+    option_arrival_s.value = city;
+    option_arrival_s.textContent = city;
+    arrival_s.appendChild(option_arrival_s);
+  });
+}
 
 // script login and register
 
@@ -48,3 +48,6 @@ signUpBtn.addEventListener("click", () => {
 signInBtn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+// dropify
+$('.dropify').dropify();
